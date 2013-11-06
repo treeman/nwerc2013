d4db8a88d8b070d7770bba11ea21e4bf94c4023d
Jonas Hietala <tradet.h@gmail.com>
2013-11-06 19:54:37 +0100

    Euler tour printing.

---
+++ lib/chinese_postman.cpp
@@ -0,0 +1,31 @@
+// Weight of euler tour in connected graph.
+// Need to fill d[][] with min cost between any two nodes. Do floyd warshall before.
+
+int memo[1 << MAX]; // dp bitmask memo structure
+
+// Min cost of increasing by one the degree of set of the given odd vertices, to make them even.
+int min_cost(int s) {
+    if (s == 0) return 0;
+    if (memo[s] != 0) return memo[s];
+
+    int best = -1;
+
+    int x = 0; // Choose our first node to switch as the first node with odd values we can find.
+    while (((s >> x) & 1) == 0) ++x; // x = number of trailing zeros
+
+    // Try to combine with all other odd value nodes.
+    for (int y = x + 1; y < n; ++y) {
+        if ((s >> y) & 1 == 0) continue;
+
+        int comb = s ^ (1 << x) ^ (1 << y); // Switch off the selected nodes.
+
+        // Cost will be to combine these two nodes + combining the rest.
+        int cost = d[x][y] + min_cost(comb);
+
+        if (best == -1 || cost < best)
+            best = cost;
+    }
+
+    return memo[s] = best;
+}
+
--- lib/euler.cpp
+++ lib/euler.cpp
@@ -1,32 +1,25 @@
-// Weight of euler tour in connected graph.
-// Need to fill d[][] with min cost between any two nodes. Do floyd warshall before.
+list<int> cyc; // list for fast insertion in middle
 
-int memo[1 << MAX]; // dp bitmask memo structure
-
-// Min cost of increasing by one the degree of set of the given odd vertices, to make them even.
-int min_cost(int s) {
-    if (s == 0) return 0;
-    if (memo[s] != 0) return memo[s];
-
-    int best = -1;
-
-    int x = 0; // Choose our first node to switch as the first node with odd values we can find.
-    while (((s >> x) & 1) == 0) ++x; // x = number of trailing zeros
-
-    // Try to combine with all other odd value nodes.
-    for (int y = x + 1; y < n; ++y) {
-        if ((s >> y) & 1 == 0) continue;
-
-        int comb = s ^ (1 << x) ^ (1 << y); // Switch off the selected nodes.
-
-        // Cost will be to combine these two nodes + combining the rest.
-        int cost = d[x][y] + min_cost(comb);
-
-        if (best == -1 || cost < best)
-            best = cost;
+void EulerTour(list<int>::iterator i, int u) {
+    for (int j = 0; j < (int)AdjList[u].size(); ++j) {
+        ii v = AdjList[u][j];
+        if (v.second) {
+            v.second = 0; // mark as to be removed
+            for (int k = 0; k < (int)AdjList[v.first].size(); ++k) {
+                ii uu = AdjList[v.first][k]; // remove bi-directional
+                if (uu.first == u && uu.second) {
+                    uu.second = 0;
+                    break;
+                }
+            }
+            EulerTour(cyc.insert(i, u), v.first);
+        }
     }
-
-    return memo[s] = best;
 }
 
+// inside main
+cyc.clear();
+EulerTour(cyc.begin(), A); // cyc contains euler tour starting at A
+for (list<int>::iterator it = cyc.begin(); it != cyc.end(); ++it)
+    printf("%d\n", *it); // the Euler tour
 
